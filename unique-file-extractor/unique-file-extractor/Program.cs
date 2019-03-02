9330618aba4c9595ba73fcd165496b8390cf48aa
aiime <gtfxtiro@protonmail.com>
2019-03-02 22:14:22 +0500

    Add comparison by hashes

--- unique-file-extractor/unique-file-extractor/Program.cs
+++ unique-file-extractor/unique-file-extractor/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace UniqueFilesExtractor
 {
@@ -9,6 +10,7 @@ namespace UniqueFilesExtractor
     {
         const string INPUT_FOLDER = "input_folder";
         const string OUTPUT_FOLDER = "output_folder";
+        const string COMPARISON_TYPE = "comparison_type";
         const string FILE_FORMAT_SPECIFIED = "file_format_specified";
         const string FILE_FORMAT = "file_format";
 
@@ -29,8 +31,16 @@ namespace UniqueFilesExtractor
                 files = GetFiles(fromFolder: config.FindConfigValue(INPUT_FOLDER));
             }
 
-            ExtractUniqueFiles(files, outputFolder: config.FindConfigValue(OUTPUT_FOLDER));
+            if (config.FindConfigValue(COMPARISON_TYPE) == "bytes")
+            {
+                ExtractUniqueFiles(files, outputFolder: config.FindConfigValue(OUTPUT_FOLDER));
+            }
+            else
+            {  
+                ExtractUniqueFiles(CalculateHashCodes(files), outputFolder: config.FindConfigValue(OUTPUT_FOLDER));
+            }
 
+            Console.WriteLine("End");
             Console.ReadKey();
         }
 
@@ -61,6 +71,28 @@ namespace UniqueFilesExtractor
             return GetFiles(fromFolder, "*");
         }
 
+        static Dictionary<byte[], FileInfo> CalculateHashCodes(List<FileInfo> files)
+        {
+            Dictionary<byte[], FileInfo> fileByHash = new Dictionary<byte[], FileInfo>(files.Count);
+
+            for (int i = 0; i < files.Count; i++)
+            {
+                Console.WriteLine("File: " + files[i].FullName);
+
+                using (FileStream stream = File.OpenRead(files[i].FullName))
+                {
+                    byte[] hash = MD5.Create().ComputeHash(stream);
+                    fileByHash.Add(hash, files[i]);
+
+                    Console.Write("Hash: ");
+                    foreach (byte hashByte in hash) Console.Write(hashByte);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+            }
+            return fileByHash;
+        }
+
         static void ExtractUniqueFiles(List<FileInfo> files, string outputFolder)
         {
             if (files.Capacity == 0)
@@ -84,7 +116,12 @@ namespace UniqueFilesExtractor
                 {
                     byte[] fileToCompareBytes = File.ReadAllBytes(fileToCompare.FullName);
 
-                    if (fileBytes.SequenceEqual(fileToCompareBytes) && !filesChecked.Contains(fileToCompare))
+                    if (fileBytes.Length != fileToCompareBytes.Length)
+                    {
+                        continue;
+                    }
+
+                    if (!filesChecked.Contains(fileToCompare) && fileBytes.SequenceEqual(fileToCompareBytes))
                     {
                         if (identicalFound == false)
                         {
@@ -103,7 +140,6 @@ namespace UniqueFilesExtractor
                             Console.WriteLine("\t" + "Size: " + fileToCompare.Length);
                             Console.WriteLine();
                         }
-
                         filesChecked.Add(fileToCompare);
                     }
                 }
@@ -126,6 +162,85 @@ namespace UniqueFilesExtractor
                     File.Copy(file.FullName, outputFolder + @"\" + file.Name);
                 }            
                 filesChecked.Add(file);
+            }
+        }
+
+        static void ExtractUniqueFiles(Dictionary<byte[], FileInfo> fileByHash, string outputFolder)
+        {
+            if (fileByHash.Count == 0)
+            {
+                Console.WriteLine("There are no files.");
+                return;
+            }
+
+            Directory.CreateDirectory(outputFolder);
+
+            List<byte[]> checkedHashes = new List<byte[]>();
+
+            foreach (KeyValuePair<byte[], FileInfo> fileByHashEntry in fileByHash)
+            {
+                if (checkedHashes.Contains(fileByHashEntry.Key)) continue;
+                
+                bool identicalFound = false;
+
+                foreach (KeyValuePair<byte[], FileInfo> fileByHashEntryToCompare in fileByHash)
+                {
+                    if (fileByHashEntry.Key.Length != fileByHashEntryToCompare.Key.Length)
+                    {
+                        continue;
+                    }
+
+                    if (!checkedHashes.Contains(fileByHashEntryToCompare.Key) && fileByHashEntry.Key.SequenceEqual(fileByHashEntryToCompare.Key))
+                    {
+                        if (identicalFound == false)
+                        {
+                            Console.WriteLine("[original]");
+                            Console.WriteLine("Name: " + fileByHashEntry.Value.Name);
+                            Console.WriteLine("Path: " + fileByHashEntry.Value.FullName);
+                            foreach (byte hashByte in fileByHashEntry.Key) Console.Write(hashByte); Console.WriteLine();
+                            Console.WriteLine("Size: " + fileByHashEntry.Value.Length);
+                            Console.WriteLine();
+                            identicalFound = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\t" + "[duplicate]");
+                            Console.WriteLine("\t" + "Name: " + fileByHashEntryToCompare.Value.Name);
+                            Console.WriteLine("\t" + "Path: " + fileByHashEntryToCompare.Value.FullName);
+                            foreach (byte hashByte in fileByHashEntryToCompare.Key) Console.Write(hashByte); Console.WriteLine();
+                            Console.WriteLine("\t" + "Size: " + fileByHashEntryToCompare.Value.Length);
+                            Console.WriteLine();
+                        }
+
+                        checkedHashes.Add(fileByHashEntryToCompare.Key);
+                    }
+                }
+
+                if (File.Exists(outputFolder + @"\" + fileByHashEntry.Value.Name))
+                {
+                    int copyIndex = 1;
+                    string newPathWithCopyIndex = String.Format(@"{0}\{1}({2}){3}", 
+                                                                outputFolder, 
+                                                                Path.GetFileNameWithoutExtension(fileByHashEntry.Value.FullName), 
+                                                                copyIndex,
+                                                                fileByHashEntry.Value.Extension);
+
+                    while (File.Exists(newPathWithCopyIndex) == true)
+                    {
+                        copyIndex++;
+                        newPathWithCopyIndex = String.Format(@"{0}\{1}({2}){3}", 
+                                                             outputFolder, 
+                                                             Path.GetFileNameWithoutExtension(fileByHashEntry.Value.FullName), 
+                                                             copyIndex,
+                                                             fileByHashEntry.Value.Extension);
+                    }
+                    File.Copy(fileByHashEntry.Value.FullName, newPathWithCopyIndex);
+                }
+                else
+                {
+                    File.Copy(fileByHashEntry.Value.FullName, outputFolder + @"\" + fileByHashEntry.Value.Name);
+                }
+                checkedHashes.Add(fileByHashEntry.Key);
             }
         }
     }
